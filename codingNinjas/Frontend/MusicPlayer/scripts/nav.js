b628beec13f96f4f5227ae0d30da22adba06b058
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-14 23:48:07 +0530

    Re: Header Layout, Content and responsiveness done.

---
+++ codingNinjas/Frontend/MusicPlayer/scripts/nav.js
@@ -0,0 +1,31 @@
+/* 
+    Handles Nav menu for mobile screens (via hamburger icon)
+*/
+
+const navList = document.querySelector(".nav-list");
+const navToggleBtn = document.querySelector(".nav-toggle-mobile");
+
+navToggleBtn.addEventListener('click',()=>{
+    const isVisibile = navList.getAttribute("data-visibility");
+    if(isVisibile==="true"){
+        navList.setAttribute("data-visibility",false);
+        navToggleBtn.setAttribute("aria-expanded",false);
+    }else{
+        navList.setAttribute('data-visibility', true);
+        navToggleBtn.setAttribute('aria-expanded', true);
+    }
+});
+
+/*
+    Handles Stopping of animations whenever window resizing occurs. If this is not included,
+    then mobile-nav menu can quickly appear & dissappear for a while. This removes that.
+    Referred from : https://css-tricks.com/stop-animations-during-window-resizing/
+*/
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
