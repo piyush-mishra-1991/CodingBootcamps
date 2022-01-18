715baad5408a6c306821a5cc20db723b515e90e5
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-01-18 16:33:18 +0530

    Template JSON data added.

---
+++ codingNinjas/Frontend/MusicPlayer/assets/data/artists-data.js
@@ -0,0 +1,107 @@
+// Contains data for Artists, their songs, related similar artist and songs.
+const artistsJSONdata = [
+    {
+        artistId: 1,
+        name: 'Sonu-Nigam',
+        imgUrl: 'https://images.pexels.com/photos/5650905/pexels-photo-5650905.jpeg?auto=compress&cs=tinysrgb&h=130',
+        numFollowers: 456,
+        numTracks: 123,
+        numAlbums: 321,
+        shortDescription: 'Indian Singer',
+        longDescription:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        songs: [
+            {
+                songName: 'A Song Name',
+                albumName: 'Album Name',
+                duration: '04:40',
+            },
+            {
+                songName: 'A 2nd Song Name',
+                albumName: 'Album Name',
+                duration: '04:40',
+            },
+            {
+                songName: 'A 3rd Song Name',
+                albumName: 'Album Name',
+                duration: '04:40',
+            },
+        ],
+        similarArtists: [
+            {
+                name: 'Artist Name',
+                numFollowers: 678,
+            },
+            {
+                name: '2nd Artist Name',
+                numFollowers: 985,
+            },
+        ],
+        similarSongs: [
+            {
+                songName: 'A Song Name',
+                songAlbum: 'Album Name',
+            },
+            {
+                songName: 'A 2nd Song Name',
+                songAlbum: 'Album Name',
+            },
+        ],
+    },
+
+    {
+        artistId: 2,
+        name: 'Nigam-Sonu',
+        imgUrl: 'https://images.pexels.com/photos/5650905/pexels-photo-5650905.jpeg?auto=compress&cs=tinysrgb&h=130',
+        numFollowers: 456,
+        numTracks: 123,
+        numAlbums: 321,
+        shortDescription: 'Indian Singer',
+        longDescription:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        songs: [
+            {
+                songName: 'A Song Name',
+                albumName: 'Album Name',
+                duration: '04:40',
+            },
+            {
+                songName: 'A 2nd Song Name',
+                albumName: 'Album Name',
+                duration: '04:40',
+            },
+            {
+                songName: 'A 3rd Song Name',
+                albumName: 'Album Name',
+                duration: '04:40',
+            },
+        ],
+        similarArtists: [
+            {
+                name: 'Artist Name',
+                numFollowers: 678,
+            },
+            {
+                name: '2nd Artist Name',
+                numFollowers: 985,
+            },
+        ],
+        similarSongs: [
+            {
+                songName: 'A Song Name',
+                songAlbum: 'Album Name',
+            },
+            {
+                songName: 'A 2nd Song Name',
+                songAlbum: 'Album Name',
+            },
+        ],
+    }
+];
+
+const getArtistDataByID = (artistId) => {
+    const foundIndex = artistsJSONdata.findIndex((artistData) => {
+        return artistData.artistId == artistId;
+    });
+    console.log(artistsJSONdata[foundIndex]);
+};
---
+++ codingNinjas/Frontend/MusicPlayer/assets/data/shared-data.js
@@ -0,0 +1,59 @@
+// Contains data for homepage: Popular Artists, Latest releases, Playlists, Radio Stations, Genres, Favorites, and Carousel data
+const sharedData = {
+    playlistData: [
+        {
+            songName: '1 playlist song',
+            artistName: 'playlist song',
+            length: '06:78',
+            releaseDate: 'JUL 5, 2014',
+            imgUrl: 'https://images.pexels.com/photos/534283/pexels-photo-534283.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+        {
+            songName: '2 playlist song',
+            artistName: 'playlist song',
+            length: '06:42',
+            releaseDate: 'JUL 9, 2014',
+            imgUrl: 'https://images.pexels.com/photos/534283/pexels-photo-534283.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+    ],
+    popularArtistsData: [
+        {
+            artistName: 'Artist 1 Name',
+            imgUrl: 'https://images.pexels.com/photos/534283/pexels-photo-534283.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+        {
+            artistName: 'Artist 2 Name',
+            imgUrl: 'https://images.pexels.com/photos/534283/pexels-photo-534283.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+    ],
+    genresData: [
+        {
+            genreName: 'Electronic',
+            imgUrl: 'https://images.pexels.com/photos/1540319/pexels-photo-1540319.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+        {
+            genreName: 'Electronic',
+            imgUrl: 'https://images.pexels.com/photos/1540319/pexels-photo-1540319.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+    ],
+    radiosData: [
+        {
+            radioName: 'Radio-PM',
+            imgUrl: 'https://images.pexels.com/photos/4480527/pexels-photo-4480527.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+        {
+            radioName: 'Radio-AM',
+            imgUrl: 'https://images.pexels.com/photos/4480527/pexels-photo-4480527.jpeg?auto=compress&cs=tinysrgb&h=130',
+        },
+    ],
+    carouselData: [
+        {
+            imgUrl: 'https://images.pexels.com/photos/210887/pexels-photo-210887.jpeg?auto=compress&cs=tinysrgb&h=350',
+        },
+        {
+            imgUrl: 'https://images.pexels.com/photos/5650956/pexels-photo-5650956.jpeg?auto=compress&cs=tinysrgb&h=350',
+        }
+    ],
+};
+
+console.log(sharedData);
